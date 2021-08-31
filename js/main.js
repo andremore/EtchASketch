432b76d031068adf88a4d02a05e998e813442a54
Andre Morgado <andremorgado.work@gmail.com>
2021-08-31 16:19:28 +0100

    Separating JS files into modules

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 // TODO: Passar HTML Inline Styles para CSS
 // TODO: Refactor
 
+import pickr from './colorPicker.js';
+
 const container = document.querySelector('#sketch');
 
 const slider = document.querySelector('input');
@@ -13,7 +15,7 @@ slider.oninput = function () {
     output.innerHTML = this.value;
 
     for (element of document.querySelectorAll('div')) {
-        container.removeChild(element);
+        container.remove(element);
     }
     grid();
 };
@@ -58,45 +60,3 @@ function grid() {
 }
 
 grid();
-
-const pickr = Pickr.create({
-    el: '.color-picker',
-    theme: 'nano', // or 'monolith', or 'nano',
-    closeOnScroll: false,
-
-    // swatches: [
-    //     'rgba(244, 67, 54, 1)',
-    //     'rgba(233, 30, 99, 0.95)',
-    //     'rgba(156, 39, 176, 0.9)',
-    //     'rgba(103, 58, 183, 0.85)',
-    //     'rgba(63, 81, 181, 0.8)',
-    //     'rgba(33, 150, 243, 0.75)',
-    //     'rgba(3, 169, 244, 0.7)',
-    //     'rgba(0, 188, 212, 0.7)',
-    //     'rgba(0, 150, 136, 0.75)',
-    //     'rgba(76, 175, 80, 0.8)',
-    //     'rgba(139, 195, 74, 0.85)',
-    //     'rgba(205, 220, 57, 0.9)',
-    //     'rgba(255, 235, 59, 0.95)',
-    //     'rgba(255, 193, 7, 1)',
-    // ],
-
-    components: {
-        // Main components
-        preview: true,
-        opacity: true,
-        hue: true,
-
-        // Input / output Options
-        interaction: {
-            hex: true,
-            rgba: true,
-            hsla: true,
-            hsva: true,
-            cmyk: true,
-            input: true,
-            clear: true,
-            save: true,
-        },
-    },
-});
